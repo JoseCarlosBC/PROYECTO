cb712eb236f4af8be480d3b57e211cdcb0f51d0c
José Carlos Barrios Chitiquez <201800376@colsanjose.edu.gt>
2020-04-06 18:25:18 -0600

    se agrego un texto de prueba

---
+++ Ricardo Milos Sobre Hielo/Ricardo Milos Sobre Hielo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ricardo_Milos_Sobre_Hielo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ricardo Milos sobre Hielo");
+
+            Console.ReadKey();
+        }
+    }
+}
